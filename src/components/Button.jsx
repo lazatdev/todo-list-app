2d537ff53258527d3a3d3279e31e56911e85bdb1
Adam Lazat <adamdeveloper29@gmail.com>
2023-01-15 10:16:19 +0100

    Btn =>  Display all todos

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
-let Button = ({backgroundColor, fontColor, content}) =>
+let Button = ({backgroundColor, fontColor, content, setIsAll}) =>
 {
     let btnStyle = {
         backgroundColor: backgroundColor,
         color: fontColor
     }
+
+    let createBtn = () =>
+    {
+        switch(content)
+        {
+            case 'All': return (<button style={btnStyle} onClick={() => setIsAll(true)}>{content}</button>);
+            default: return (<button style={btnStyle}>{content}</button>);
+        }
+    }
     return (
-        <button style={btnStyle}>{content}</button>
+        <>
+            {createBtn()}
+        </>
     )
 }
 
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import SingleTodo from './SingleTodo';
+import Button from './Button';
 
 let TodoItems = ({todos, getNumberOfTodos}) =>
 {
+    let [isAll, setIsAll] = useState(false);
+
     return (
         <>
             <h3>Todo List</h3>
             <div className="numberOfTodos">
                 {`${getNumberOfTodos()} Todos`}
             </div>
+            <div className="display-todos">
+                <Button content={'All'} setIsAll={setIsAll}/>
+            </div>
             <ul className="todo-items">
                 {
-                    todos.map((todo, index) => {
+                    isAll ? todos.map((todo, index) => {
                         return <SingleTodo key={index} todo={todo}/>
-                    })
+                    }) : ''
                 }
             </ul>
         </>
-
     )
 }
 
